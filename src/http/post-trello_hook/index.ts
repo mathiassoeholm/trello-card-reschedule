e14fc82a661d88525b01df2b6b07535710d652da
Mathias Soeholm <mathiassoeholm@gmail.com>
2021-01-10 20:14:00 +0100

    Add a trello-hook endpoint

---
+++ src/http/post-trello_hook/index.ts
@@ -0,0 +1,11 @@
+export async function handler(req: object) {
+  // Verify Trello signature, return unauthorized if not match
+  // Get all cards in the done column
+  // Change due data based on some sort of `days_between` field
+  // Move the card back to the todo column
+  // Sort todo column based on due date
+
+  return {
+    statusCode: 200,
+  };
+}
