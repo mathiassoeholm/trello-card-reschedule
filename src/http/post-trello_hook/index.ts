0fa9bb8d100024a9ffb74c602fff1d29d253a4e9
Mathias Soeholm <mathiassoeholm@gmail.com>
2021-01-11 17:29:57 +0100

    Log the request body

--- src/http/post-trello_hook/index.ts
+++ src/http/post-trello_hook/index.ts
@@ -1,10 +1,11 @@
-export async function handler(req: object) {
+export async function handler(req: any) {
   // Verify Trello signature, return unauthorized if not match
   // Get all cards in the done column
   // Change due data based on some sort of `days_between` field
   // Move the card back to the todo column
   // Sort todo column based on due date
-  console.log("Req", req);
+  console.log("api key", Deno.env.get("TRELLO_API_KEY"));
+  console.log(JSON.stringify(req.body));
 
   return {
     statusCode: 200,
