fe4cebd525266fe4d73cd4f303df10cb62acc882
Mathias Soeholm <mathiassoeholm@gmail.com>
2021-01-12 17:26:16 +0100

    Verify that call comes from Trello

--- src/http/post-trello_hook/index.ts
+++ src/http/post-trello_hook/index.ts
@@ -1,13 +1,43 @@
+import { hmac } from "https://denopkg.com/chiefbiiko/hmac/mod.ts";
+
 export async function handler(req: any) {
   // Verify Trello signature, return unauthorized if not match
   // Get all cards in the done column
   // Change due data based on some sort of `days_between` field
   // Move the card back to the todo column
   // Sort todo column based on due date
-  console.log("api key", Deno.env.get("TRELLO_API_KEY"));
-  console.log(JSON.stringify(req.body));
+  console.log(req);
 
+  if (
+    !verifyTrelloWebhookRequest(
+      req,
+      Deno.env.get("TRELLO_APP_SECRET")!,
+      Deno.env.get("CALLBACK_URL")!
+    )
+  ) {
+    console.log("403, not authorized");
+    return {
+      statusCode: 403,
+    };
+  }
+
+  console.log("200, ok");
   return {
     statusCode: 200,
   };
+}
+
+function verifyTrelloWebhookRequest(
+  req: any,
+  secret: string,
+  callbackURL: string
+) {
+  const content = JSON.stringify(req.body) + callbackURL;
+  const hash = hmac("sha1", secret, content, "utf8", "base64");
+  const headerHash = req.headers["x-trello-webhook"];
+
+  console.log("hash", hash);
+  console.log("headerHash", headerHash);
+
+  return hash == headerHash;
 }
