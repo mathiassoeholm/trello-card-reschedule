b44ca6056f8c93381b64ef0433c79c0dd983c816
Mathias Soeholm <mathiassoeholm@gmail.com>
2021-01-12 18:36:48 +0100

    Start parsing the days-between field on tasks in done

--- src/http/post-trello_hook/index.ts
+++ src/http/post-trello_hook/index.ts
@@ -1,19 +1,23 @@
 import { hmac } from "https://denopkg.com/chiefbiiko/hmac/mod.ts";
 
 export async function handler(req: any) {
-  // Verify Trello signature, return unauthorized if not match
-  // Get all cards in the done column
   // Change due data based on some sort of `days_between` field
   // Move the card back to the todo column
   // Sort todo column based on due date
   console.log(req);
 
+  const development = Deno.env.get("DEVELOPMENT");
+  const boardId = Deno.env.get("BOARD_ID");
+  const doneListId = Deno.env.get("DONE_LIST_ID");
+  const todoListId = Deno.env.get("TODO_LIST_ID");
+  const trelloApiToken = Deno.env.get("TRELLO_API_TOKEN");
+  const trelloApiKey = Deno.env.get("TRELLO_API_KEY");
+  const trelloAppSecret = Deno.env.get("TRELLO_APP_SECRET") ?? "";
+  const callbackUrl = Deno.env.get("CALLBACK_URL") ?? "";
+
   if (
-    !verifyTrelloWebhookRequest(
-      req,
-      Deno.env.get("TRELLO_APP_SECRET")!,
-      Deno.env.get("CALLBACK_URL")!
-    )
+    !development &&
+    !verifyTrelloWebhookRequest(req, trelloAppSecret, callbackUrl)
   ) {
     console.log("403, not authorized");
     return {
@@ -21,7 +25,36 @@ export async function handler(req: any) {
     };
   }
 
-  console.log("200, ok");
+  const cards = await fetch(
+    `https://api.trello.com/1/boards/${boardId}/cards?key=${trelloApiKey}&token=${trelloApiToken}`
+  );
+
+  const cardsInDone: any[] = (await cards.json()).filter(
+    (card: any) => card.idList === doneListId
+  );
+
+  const handleDoneCards = cardsInDone.map((card: any) =>
+    (async () => {
+      const daysBetween = parseInt(
+        (card.desc as string | null)?.match(/\[days-between=(\d+)\]/)?.[1] ??
+          "1"
+      );
+
+      console.log("daysBetween", daysBetween);
+
+      // await fetch(
+      //   `https://api.trello.com/1/cards/${card.id}?key=${trelloApiKey}&token=${trelloApiToken}'`,
+      //   {
+      //     method: "PUT",
+      //   }
+      // );
+    })()
+  );
+
+  await Promise.all(handleDoneCards);
+
+  console.log(cardsInDone);
+
   return {
     statusCode: 200,
   };
@@ -33,13 +66,8 @@ function verifyTrelloWebhookRequest(
   callbackURL: string
 ) {
   const content = req.body + callbackURL;
-  console.log("content");
-  console.log(content);
   const hash = hmac("sha1", secret, content, "utf8", "base64");
   const headerHash = req.headers["x-trello-webhook"];
-
-  console.log("hash", hash);
-  console.log("headerHash", headerHash);
 
   return hash == headerHash;
 }
